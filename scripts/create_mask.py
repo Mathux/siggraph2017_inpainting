e9b8d3c0312d308d3f04e1e240380d1a13f6e256
Mathis Petrovich <mathis.petrovich@gmail.com>
2018-12-14 17:57:32 +0100

    add forms and toggle

--- scripts/create_mask.py
+++ scripts/create_mask.py
@@ -18,15 +18,22 @@ class Mode(Enum):
     REMOVE = 1
 
 
+class Form(Enum):
+    RECT = 0
+    CIRCLE = 1
+
+
+def toggle_form(mode):
+    Forms = list(Form)
+    return Forms[(mode.value + 1) % len(Forms)]
+
 def toggle_mode(mode):
-    if mode==Mode.ADD:
-        return Mode.REMOVE
-    elif mode==Mode.REMOVE:
-        return Mode.ADD
-    else:
-        print("Cannot happend toggle_mode")
-        return None
-    
+    Modes = list(Mode)
+    return Modes[(mode.value + 1) % len(Modes)]
+
+
+sq2_2 = 0.707107
+
 white = (255,255,255)
 black = (0,0,0)
 blue = (255,0,0)
@@ -41,40 +48,50 @@ colors_action[Mode.ADD] = white
 colors_action[Mode.REMOVE] = black
 
 
-def circle_on_mask(mask, x, y, radus, color, fill=-1):
-    cv2.circle(mask, (x,y), radus, color, fill)
-
+def form_on_mask(param, mask_name, fill=-1):
+    mask = param[mask_name]
+    x, y = param["x"], param["y"]
+    radus = param["radus"]
+    mode = param["mode"]
+    form = param["form"]
+    color = colors_action[mode] if mask_name=="mask" else colors_cursor[mode]        
+        
+    if form==Form.CIRCLE:
+        cv2.circle(mask, (x,y), radus, color, fill)
+        
+    elif form==Form.RECT:
+        x1, y1 = int(x - sq2_2*radus), int(y - sq2_2*radus)
+        x2, y2 = int(x + sq2_2*radus), int(y + sq2_2*radus)
+        cv2.rectangle(mask, (x1, y1), (x2, y2), color, fill)
 
 # mouse callback function
-def draw_circle(event, x, y, flags, param):
-    mask = param["mask"]
-    param["maskr"] = param["maskr"]*0
+def draw_form(event, x, y, flags, param):
     mode = param["mode"]
     param["x"] = x
     param["y"] = y
+
+    # Remove old movement
+    param["mask_cur"] = param["mask_cur"]*0
     
     color_cursor = colors_cursor[mode]
-    circle_on_mask(param["maskr"], x, y, param["radus"], color_cursor)
+    form_on_mask(param, "mask_cur")
 
     if event == cv2.EVENT_LBUTTONDOWN:
         param["drawing"] = True
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if param["drawing"] == True:
-            color_action = colors_action[mode]
-            circle_on_mask(param["mask"], x, y, param["radus"], color_action)
+            form_on_mask(param, "mask")
         
     elif event == cv2.EVENT_LBUTTONUP:
         param["drawing"] = False
-        color_action = colors_action[mode]
-        circle_on_mask(param["mask"], x, y, param["radus"], color_action)
+        form_on_mask(param, "mask")
 
-            
 
-def mix_images(image, mask, maskr, mode):
+def mix_images(image, mask, mask_cur, mode):
     mix = np.copy(image)
     mix[mask==white] = 255
-    cursor = (maskr==colors_cursor[mode]).all(axis=2)
+    cursor = (mask_cur==colors_cursor[mode]).all(axis=2)
     mix[cursor] = colors_cursor[mode]
     return mix
 
@@ -86,6 +103,7 @@ def save_mask(mask, path):
 def print_help(impath, outpath):
     print("This is a tool to create a mask from an input image")
     print("-> To change mode (add/remove) press t")
+    print("-> To change form (circle/rectangle) press f")
     print("-> To save the image, press s")
     print("-> To change radus, use the trackbar")
     print("-> To quit, press ESQ or q")
@@ -109,16 +127,18 @@ if __name__ == "__main__":
     
     param = {}
     param["mask"] = np.zeros_like(image)
-    param["maskr"] = np.zeros_like(image)
+    param["mask_cur"] = np.zeros_like(image)
     param["mode"] = Mode.ADD
     
     param["drawing"] = False
     param["radus"] = 20
     param["x"] = -1
     param["y"] = -1
+
+    param["form"] = Form.CIRCLE
     
     cv2.namedWindow(name)
-    cv2.setMouseCallback(name, draw_circle, param)
+    cv2.setMouseCallback(name, draw_form, param)
 
     def trackbar_callback(radus):
         param["radus"] = radus
@@ -127,7 +147,7 @@ if __name__ == "__main__":
 
 
     while(1):
-        mix = mix_images(image, param["mask"], param["maskr"], param["mode"])
+        mix = mix_images(image, param["mask"], param["mask_cur"], param["mode"])
 
         cv2.imshow(name, mix)
         k = cv2.waitKey(1) & 0xFF
@@ -137,7 +157,14 @@ if __name__ == "__main__":
 
         elif k==ord("t"):            
             param["mode"] = toggle_mode(param["mode"])
-            circle_on_mask(param["maskr"], param["x"], param["y"], param["radus"], colors_cursor[param["mode"]])
+            param["mask_cur"] = param["mask_cur"]*0
+            form_on_mask(param, "mask_cur")
+            
+        elif k==ord("f"):
+            param["form"] = toggle_form(param["form"])
+            param["mask_cur"] = param["mask_cur"]*0
+            form_on_mask(param, "mask_cur")
+            
         elif k==ord("s"):
             print(outpath)
             save_mask(param["mask"
